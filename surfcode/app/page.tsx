142ec4342b48a85254a786e4cc32aa388c1629bc
Pedro Morais <88340056+Pedros-Morais@users.noreply.github.com>
2024-08-26 13:00:56 -0300

    ShadCn

--- surfcode/app/page.tsx
+++ surfcode/app/page.tsx
@@ -1,9 +1,8 @@
-
+import { Button } from "@/components/ui/button"
 const Home = () => {
-
   return (
     <div className=" text-black">
-      SurfCode WhiteLabel
+      <Button>Surf Code</Button>
     </div>
   );
 }
